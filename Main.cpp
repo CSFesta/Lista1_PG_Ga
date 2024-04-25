59c1dc6e6c4c6e7dd55cc011e4f94ddb485513ce
Caua-Festa <100861169+CSFesta@users.noreply.github.com>
2024-04-24 23:19:55 -0300

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -5,16 +5,43 @@ void drawTriangle() {
     static int size = 3;
     float triangulo1[3][2]{ {-0.5,0.5}, {-0.5,-0.5} ,{0.5,0.5} };
     float triangulo2[3][2]{ {-0.5,-0.5}, {0.5,-0.5} ,{0.5,0.5} };
+
+
+    glBegin(GL_TRIANGLES);
+    for (const auto& i  : triangulo1) {
+        glColor3f(1, 0, 0);
+        glVertex2f(i[0],i[1]);
+
+    }
+    for (const auto& i : triangulo2) {
+        glColor3f(0, 0, 1);
+        glVertex2f(i[0], i[1]);
+
+    }
+    glEnd();
+
     glPointSize(10.0f);
-
     glBegin(GL_POINTS);
-    for (int i = 0; i < size; i++) {
-
-        glColor3f(1, 1, 0);
-        glVertex2f(triangulo1[i][0], triangulo1[i][1]);
-
+    for (const auto& i : triangulo1) {
         glColor3f(0, 1, 0);
-        glVertex2f(triangulo2[i][0], triangulo2[i][1]);
+        glVertex2f(i[0], i[1]);
+    }
+    for (const auto& i : triangulo2) {
+        glColor3f(0, 1, 0);
+        glVertex2f(i[0], i[1]);
+    }
+    glEnd();
+
+    glBegin(GL_LINE_STRIP);
+
+    for (int i = 0; i < size; i++) {
+        glColor3f(1, 1, 1);
+        glVertex2f(triangulo1[i % size][0], triangulo1[i % size][1]);
+        glVertex2f(triangulo1[(i + 1) % size][0], triangulo1[(i + 1) % size][1]);
+
+        glColor3f(1, 1, 1);
+        glVertex2f(triangulo2[i % size][0], triangulo2[i % size][1]);
+        glVertex2f(triangulo2[(i + 1) % size][0], triangulo2[(i + 1) % size][1]);
     }
     glEnd();
 
